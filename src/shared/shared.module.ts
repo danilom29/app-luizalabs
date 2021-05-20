055c602ee27ae0ce4955ba5230c785110585e2ad
danilom29 <danilo_m29@hotmail.com>
2021-05-20 15:19:58 -0300

    feat: CRUD do modulo de clientes

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ItemListCardComponent } from './components/item-list-card/item-list-card.component';
 import { ItemListContentComponent } from './components/item-list-content/item-list-content.component';
 import { ItemListHeaderComponent } from './components/item-list-header/item-list-header.component';
 import { ItemListFooterComponent } from './components/item-list-footer/item-list-footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { TitleComponent } from './components/title/title.component';
 import { IonicModule } from '@ionic/angular';
 @NgModule({
   declarations: [
-    ItemListCardComponent,
     ItemListContentComponent,
     ItemListHeaderComponent,
     ItemListFooterComponent,
-    TitleComponent,
   ],
   imports: [CommonModule, RouterModule, ReactiveFormsModule, IonicModule],
   exports: [
     ItemListContentComponent,
     ItemListHeaderComponent,
     ItemListFooterComponent,
-    ItemListCardComponent,
-    TitleComponent,
   ],
 })
 export class SharedModule {}
